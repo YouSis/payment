5f4067d36895d066bba1c3e5acd4c32bb9ef7b83
wangjingang <wangjingang@wangfujing.com>
2017-06-21 15:18:55 +0800

    集成了dubbo

--- pay-offline-gateway/src/main/java/com/wfj/pay/BootApplication.java
+++ pay-offline-gateway/src/main/java/com/wfj/pay/BootApplication.java
@@ -2,12 +2,11 @@ package com.wfj.pay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Created by wjg on 2017/6/19.
  */
-@SpringBootApplication(scanBasePackages = "com.wfj.pay",exclude ={DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.wfj.pay")
 public class BootApplication {
 
     public static void main(String[] args) {
---
+++ pay-service-interface/src/main/java/com/wfj/pay/dubbo/ITestDubbo.java
@@ -0,0 +1,8 @@
+package com.wfj.pay.dubbo;
+
+/**
+ * Created by wjg on 2017/6/21.
+ */
+public interface ITestDubbo {
+    String test(String str);
+}
